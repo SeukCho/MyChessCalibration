3c2190b7dd523833d6474f0dbbc7631bff7c5097
SeukCho <k7i8239@gmail.com>
2023-04-30 19:22:05 +0900

    up

--- pose_estimation_chessboard.py
+++ pose_estimation_chessboard.py
@@ -3,10 +3,10 @@ import cv2 as cv
 
 # The given video and calibration data
 input_file = 'chessboard.avi'
-K = np.array([[432.7390364738057, 0, 476.0614994349778],
-              [0, 431.2395555913084, 288.7602152621297],
-              [0, 0, 1]])
-dist_coeff = np.array([-0.2852754904152874, 0.1016466459919075, -0.0004420196146339175, 0.0001149909868437517, -0.01803978785585194])
+K = np.array([[434.20154504, 0, 476.69359916], 
+              [0, 432.83771324, 290.20247331],   
+              [0, 0, 1]] )
+dist_coeff = np.array([-2.88706447e-01, 1.04936709e-01, -3.03034020e-04, 2.50615605e-04, -1.90077401e-02])
 board_pattern = (10, 7)
 board_cellsize = 0.025
 board_criteria = cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_NORMALIZE_IMAGE + cv.CALIB_CB_FAST_CHECK
